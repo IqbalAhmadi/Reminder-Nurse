02d164f1cabc4605490009cab3d3ff24eeeb6064
Mohammad Ahmadi <iqbal_dyncorp@live.com>
2023-02-28 11:34:29 -0800

    created userSchema, hashed the password, & compared & validate it

---
+++ server/models/user.js
@@ -0,0 +1,35 @@
+const { Schema, model} = require('mongoose')
+const bcrypt = require('bcrypt')
+
+// creating userSchema
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true, 
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+})
+
+// hash user password
+userSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+      const saltRounds = 10;
+      this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+  
+    next();
+  });
+
+  // custom method to compare and validate password for logging in
+userSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+  };
+
+
+  const user = model('user', userSchema)
+
+  module.exports = user
