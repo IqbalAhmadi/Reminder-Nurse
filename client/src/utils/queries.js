c2e8f29b0c0bcae7b68343e22720e29be0272655
Amauri <hamauri@aol.com>
2023-02-28 14:43:28 -0800

    added client query/mutations

---
+++ client/src/utils/queries.js
@@ -0,0 +1,33 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_MEDICINE = gql`
+  query singleMedicine($medicineId: ID!) {
+    medicine(medicineId: $medicineId) {
+      _id
+      name
+      start
+      amount
+      interval
+      subInterval
+      times
+      isActive
+      userId
+    }
+  }
+`;
+
+export const QUERY_MEDICINES = gql`
+  query allMedicines {
+    medicines {
+      _id
+      name
+      start
+      amount
+      interval
+      subInterval
+      times
+      isActive
+      userId
+    }
+  }
+`;
