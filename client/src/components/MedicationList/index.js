3b90cd5a84f4daa5a4d5fba9c7b046cbac184ec7
Amauri <hamauri@aol.com>
2023-02-28 15:27:46 -0800

    cleaned up component/page for current page

--- client/src/components/MedicationList/index.js
+++ client/src/components/MedicationList/index.js
@@ -1,15 +1,9 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
-import { QUERY_MEDICINES } from '../../utils/queries';
 
-const MedicationList = ({}) => {
-  const { loading, data } = useQuery(QUERY_MEDICINES);
-
-  if (loading) return <h2>Loading...</h2>;
+const MedicationList = ({ medicines }) => {
   return (
     <ol>
-      {data.medicines.map((medicine) => {
+      {medicines.map((medicine) => {
         return <li key={medicine._id}>{medicine.name}</li>;
       })}
     </ol>
