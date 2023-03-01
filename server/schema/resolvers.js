c25b0a436be426f1f7f8f8a6ab325ab427445064
Amauri <hamauri@aol.com>
2023-02-28 20:33:10 -0800

    undo wrong stage change

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -11,11 +11,9 @@ const resolvers = {
     },
     // gets all medicine matching userId using context
     medicines: async (parent, args, context) => {
-      // if (!context.user)
-      //   throw new AuthenticationError('You need to be logged in!');
-      // return Medicine.find({ userId: context.user.id });
-
-      return Medicine.find({});
+      if (!context.user)
+        throw new AuthenticationError('You need to be logged in!');
+      return Medicine.find({ userId: context.user.id });
     },
   },
   Mutation: {
