a2152498f1943d10ec85485257c3703d00a40fbd
Amauri <hamauri@aol.com>
2023-03-01 14:54:37 -0800

    toggle isActive client side working

--- client/src/components/MedicationList/index.js
+++ client/src/components/MedicationList/index.js
@@ -1,34 +1,44 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
+import { useMutation } from '@apollo/client';
+import { TOGGLE_ACTIVE } from '../../utils/mutations';
 
 const MedicationList = ({ medicines, isActive }) => {
-  const handleMedicineToggle = async () => {};
+  const [toggleMedicine] = useMutation(TOGGLE_ACTIVE);
+
+  const handleMedicineToggle = async (e) => {
+    try {
+      const medicineId = e.target.id;
+      await toggleMedicine({ variables: { medicineId } });
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   return (
     <ol className="medication-list display-flex flex-wrap">
-      {medicines.map((medicine) => {
-        if (isActive == medicine.isActive) {
-          return (
-            <li key={medicine._id} style={{ listStyleType: 'none' }}>
-              <section className="display-flex">
-                <h3>{medicine.name}</h3>
-                <section className="m-1">
-                  <Button variant="secondary">
-                    <Link to={'../medicine/' + medicine._id}>Modify</Link>
-                  </Button>
-                  <Button
-                    variant={isActive ? 'danger' : 'success'}
-                    onClick={handleMedicineToggle}
-                  >
-                    {isActive ? 'Deactivate' : 'Activate'}
-                  </Button>
-                </section>
+      {medicines.map((medicine) =>
+        isActive === medicine.isActive ? (
+          <li key={medicine._id} style={{ listStyleType: 'none' }}>
+            <section className="display-flex">
+              <h3>{medicine.name}</h3>
+              <section className="m-1">
+                <Button variant="secondary">
+                  <Link to={'../medicine/' + medicine._id}>Modify</Link>
+                </Button>
+                <Button
+                  variant={isActive ? 'danger' : 'success'}
+                  id={medicine._id}
+                  onClick={handleMedicineToggle}
+                >
+                  {isActive ? 'Deactivate' : 'Activate'}
+                </Button>
               </section>
-            </li>
-          );
-        }
-      })}
+            </section>
+          </li>
+        ) : null
+      )}
     </ol>
   );
 };
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,1 +1,17 @@
 import { gql } from '@apollo/client';
+
+export const TOGGLE_ACTIVE = gql`
+  mutation toggleMedicine($medicineId: ID!) {
+    toggleMedicine(medicineId: $medicineId) {
+      _id
+      name
+      start
+      amount
+      interval
+      subInterval
+      times
+      isActive
+      userId
+    }
+  }
+`;
