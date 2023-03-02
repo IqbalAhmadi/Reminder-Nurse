36c3b6fa88295cbef81b303ddcb2ab9c6d519cd8
Amauri <hamauri@aol.com>
2023-03-02 15:43:28 -0800

    added 404 page

---
+++ client/src/pages/NotFound.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Container, Button } from 'react-bootstrap';
+
+const Home = () => {
+  return (
+    <Container>
+      <h2>Page not found.</h2>
+      <Link to={'daily'}>
+        <Button>Daily</Button>
+      </Link>
+      <Link to={'medicines'}>
+        <Button>Medicines</Button>
+      </Link>
+    </Container>
+  );
+};
+
+export default Home;
