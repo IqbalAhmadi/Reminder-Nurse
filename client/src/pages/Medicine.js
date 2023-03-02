d733953d2cc0203d5ee05e51a218da51f3e989cd
Amauri <hamauri@aol.com>
2023-03-01 21:12:17 -0800

    redirect /medicine/:id path if incorrect id

--- client/src/pages/Medicine.js
+++ client/src/pages/Medicine.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
 import { QUERY_MEDICINE } from '../utils/queries';
-import { useParams, Link } from 'react-router-dom';
+import { useParams, Link, Navigate } from 'react-router-dom';
 import Medication from '../components/Medication';
 import { Button } from 'react-bootstrap';
 
@@ -12,15 +12,18 @@ const Medicine = () => {
   });
 
   if (loading) return <h2>Loading...</h2>;
-  if (error) return <h2>{error.toString()}</h2>;
-
+  
   return (
     <section className="edit-medication">
       <Button>
         <Link to={'../medicines'}>Go Back</Link>
       </Button>
       <h2>Edit your medications info</h2>
-      <Medication medicine={data.medicine} />
+      {data ? (
+        <Medication medicine={data.medicine} />
+      ) : (
+        <Navigate to="../medicines" />
+      )}
     </section>
   );
 };
