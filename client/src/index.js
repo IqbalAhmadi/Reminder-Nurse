e1253b3cd5d92d2fa205cdad2ae78bda791c9418
Amauri <hamauri@aol.com>
2023-03-04 11:40:13 -0800

    notifications working

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Notifications  />
+    <Notifications />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
