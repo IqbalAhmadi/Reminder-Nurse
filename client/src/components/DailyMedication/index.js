c9e2c7a0a8c2ce7734366587bb65183666448bb4
christiangella <112913805+christiangella@users.noreply.github.com>
2023-03-05 14:15:03 -0800

    layout / media queries

--- client/src/components/DailyMedication/index.js
+++ client/src/components/DailyMedication/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Scheduler from '../../utils/scheduler';
 import 'animate.css';
-import rnStatic from '../../assets/images/rn_static_01.png';
 
 const DailyMedication = ({ medicine }) => {
   const createSchedules = async () => {
@@ -66,7 +65,7 @@ const DailyMedication = ({ medicine }) => {
       <div className="card-body px-4 pt-1 col-7">
         <h3 className="dailyHeader">{medicine.name}</h3>
         <hr />
-        <p className="dailystext">{medicine.time}</p>
+        <p className="dailystext">Take at {medicine.time}</p>
         <p className="dailyRemain">
           You have {medicine.amount} remaining dosages.
         </p>
