64867c09b8ce85796824b00249ccffc7ee6d5db7
Amauri <hamauri@aol.com>
2023-03-05 16:04:59 -0800

    fixed conflicts

--- client/src/components/DailyMedication/index.js
+++ client/src/components/DailyMedication/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Scheduler from '../../utils/scheduler';
+import rnStatic from '../../assets/images/rn_static_01.png';
 import 'animate.css';
 
 const DailyMedication = ({ medicine }) => {
@@ -11,73 +12,6 @@ const DailyMedication = ({ medicine }) => {
   createSchedules();
 
   return (
-    <div>
-      <h2 className="dmedHeader">daily medication</h2>
-      <center>
-        <div className="row dTop">
-          <div className="imgContain col-5">
-            <img
-              src={rnStatic}
-              className="imgNurse"
-              alt="Icon of the Reminder Nurse"
-            />
-          </div>
-          <div className="col-5 animate__animated animate__fadeIn">
-            <div className="card dailyDialogue border-0 shadow-sm">
-              <div className="card-body">
-                Let's take at your medications today.
-              </div>
-            </div>
-          </div>
-        </div>
-      </center>
-      <ul className="list-group">
-        {dailymeds.map((med) => {
-          return (
-            <li
-              key={med._id + med.time}
-              className="card shadow p-2 mb-4 bg-white rounded container dailyCard border-0"
-            >
-              <div className="row">
-                <div className="card-body px-4 pt-1 col-7 ">
-                  <h3 className="dailyHeader">{med.name}</h3>
-                  <hr />
-                  <p className="dailystext">{med.time}</p>
-                  <p className="dailyRemain">
-                    You have {med.amount} remaining dosages.
-                  </p>
-                </div>
-                <div className="col">
-                  <div className="form-check">
-                    <input
-                      className="form-check-input dailyCheck"
-                      type="checkbox"
-                    ></input>
-                  </div>
-                </div>
-              </div>
-            </li>
-          );
-        })}
-      </ul>
-    <div className="row">
-      <div className="card-body px-4 pt-1 col-7">
-        <h3 className="dailyHeader">{medicine.name}</h3>
-        <hr />
-        <p className="dailystext">Take at {medicine.time}</p>
-        <p className="dailyRemain">
-          You have {medicine.amount} remaining dosages.
-        </p>
-      </div>
-      <div className="col">
-        <div className="form-check">
-          <input
-            className="form-check-input dailyCheck"
-            type="checkbox"
-          ></input>
-        </div>
-      </div>
-    </div>
     <div className="row">
       <div className="card-body px-4 pt-1 col-7">
         <h3 className="dailyHeader">{medicine.name}</h3>
